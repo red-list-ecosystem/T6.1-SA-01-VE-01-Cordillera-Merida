40df46a60c3e0b8e2654cea7144d04cc9aa3b940
jrfep <jr.ferrer.paris@gmail.com>
2023-05-18 15:59:56 +1000

    export diagrams as figures for the OSF site

---
+++ inc/export-figures-from-graphviz.sh
@@ -0,0 +1,3 @@
+dot -Teps -Gsize=7,5\! -Gdpi=300 -osandbox/CEM-Venezuela-TGE.eps assets/CEM-Venezuela-TGE.gv 
+
+dot -Tpng -Gsize=10 -Gdpi=300 -osandbox/Graphical-assessment-summary.png assets/Data-sources-Venezuela.gv 
