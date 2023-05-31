876a7436b1fd43ad8582bc29699c4a36b36e8a52
jrfep <jr.ferrer.paris@gmail.com>
2023-05-31 22:30:01 +1000

    Preparing final document for submission

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # T6.1-SA-01-VE-01-Cordillera-Merida
 
-RLE assessment of the Tropical Glacier Ecosystem of the Cordillera de Mérida
+RLE assessment of the Tropical glacier ecosystem of the Cordillera de Mérida
 
 https://osf.io/y3279/
 
+## Data download
+We need to download data from the OSF cloud storage:
+
+Run the script in `config/download-data.R`
+
+## Install quarto
+
+Download from here and install: https://quarto.org/docs/get-started/
+
+## Install Java
+
+For some of the maplayers I need Java
+
+https://www.oracle.com/java/technologies/downloads/#jdk20-mac
+
+Download:
+https://download.oracle.com/java/20/latest/jdk-20_macos-aarch64_bin.dmg
+
+and Install
--- config/download-data.R
+++ config/download-data.R
@@ -1,17 +1,24 @@
+# Script to download data from OSF cloud storage
+
+## Find the root of the repository folder and create a sandbox folder
 here::i_am("config/download-data.R")
 target.dir <- "sandbox"
 if (!file.exists(here::here(target.dir)))
   dir.create(here::here(target.dir))
 
+## For this to work we need to define "OSF_PAT" in a .Renviron file
 library(osfr)
+
+## OSF project ID:
 osfid <- "n73wk"
 osf_project <- osf_retrieve_node(sprintf("https://osf.io/%s",osfid))
 
+## List all files and download them in target.dir:
 project_files <- osf_ls_files(osf_project)
-osf_download(project_files, path=here::here(target.dir))
+osf_download(project_files, path=here::here(target.dir), conflicts="overwrite")
 
+## unzip the spatial data in the target.dir:
 unzip (here::here(target.dir,"GIS-data-VEN.zip"),
        exdir=here::here(target.dir))
-
 
 # Done !
